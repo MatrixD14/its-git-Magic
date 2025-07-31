eb37b53f657c8b19298c8d33d8b2a7ff9ac5bd70
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-07-30 23:32:35 -0400

    kkkkkkkkk

---
+++ Files/Worlds/gitTest.java
@@ -0,0 +1,124 @@
+public class gitTest extends Component {
+  @Order(idx = -2)
+  public String links, paths;
+  public String Commit = "comito", toke;
+  private String tm;
+  private boolean onoff;
+
+  @Order(idx = -1)
+  public PropertiesButton DownLoad =
+      new PropertiesButton(
+          new PropertiesButtonListener() {
+            void onClicked() {
+              download();
+            }
+          });
+
+  public PropertiesButton update =
+      new PropertiesButton(
+          new PropertiesButtonListener() {
+            void onClicked() {
+              upFile();
+            }
+          });
+
+  public void download() {
+    // tm = Directories.internal() +"test/" +paths;
+    tm = Directories.getProjectFolder() + "/Files/" + paths;
+    String json = "{\n pasth:\"" + tm + "\",\n NameFile: " + paths + "\n}";
+    Console.log(json);
+    //  update(links, tm);
+  }
+
+  public void upFile() {
+    tm = Directories.getProjectFolder() + "/Files/" + paths;
+    String shas = links+ "Files/"+ paths;
+    gitpush(links + "Files/" + paths + "?ref=main", Commit, tm, toke, shas);
+    // https://api.github.com/repos/MatrixD14/game2d/contents/
+  }
+
+  public void update(String link, String path) {
+    try {
+      URL url = new URL(link);
+      InputStream in = url.openStream();
+      BufferedInputStream bs = new BufferedInputStream(in);
+      FileOutputStream fs = new FileOutputStream(path);
+      byte[] date = new byte[1024];
+      int count;
+      while ((count = bs.read(date, 0, 1024)) != -1) {
+        fs.write(date, 0, count);
+      }
+      fs.flush();
+      fs.close();
+    } catch (Exception e) {
+      Console.log(e);
+    }
+  }
+
+  public void gitpush(String link, String menssage, String pasth, String token, String sha) {
+    try {
+      byte[] date = readFile(pasth);
+      String encode = Base64.getEncoder().encodeToString(date);
+      String json = "{\n  \"message\": \"" + menssage + "\",\n\"content\": \"" + encode + "\",\n\"branch\": \"main\",\n \"sha\": \"" + sha + "\"\n}";
+      Console.log(json);
+
+      URL url = new URL(link);
+      HttpURLConnection com = (HttpURLConnection) url.openConnection();
+      com.setRequestMethod("PUT");
+      com.setDoOutput(true);
+      com.setRequestProperty("Authorization", "token " + token);
+      com.setRequestProperty("Content-Type", "application/json");
+      
+      OutputStream output = com.getOutputStream();
+      output.write(json.getBytes("UTF-8"));
+      output.flush();
+      output.close();
+      int menss = com.getResponseCode();
+      Console.log(menss == 201 || menss == 200 ? "folder enviado sucess" : "erro em algum folder");
+    } catch (Exception e) {
+      Console.log(e);
+    }
+  }
+  
+  
+  public byte[] readFile(String pasth){
+      try{
+          FileInputStream fs = new FileInputStream(pasth);
+          ByteArrayOutputStream buffer = new ByteArrayOutputStream(); 
+      byte[] date = new byte[1024];
+      int read;
+      while((read = fs.read(date)) != -1) buffer.write(date,0,read);
+      fs.close();
+      return buffer.toByteArray();
+      }catch(IOException e) {
+          Console.log(e);
+      }
+      return null;
+  }
+
+  public String getSha(String link, String token) {
+    try {
+      URL url = new URL(link);
+      HttpURLConnection com = (HttpURLConnection) url.openConnection();
+      com.setRequestMethod("GET");
+      com.setRequestProperty("Authorization", "token " + token);
+      com.setRequestProperty("Accept", "application/vnd.github.v3+json");
+      InputStream input = com.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      StringBuilder result = new StringBuilder();
+      String line;
+      while((line = reader.readLine())!= null) result.append(line);
+      
+      String json = result.toString();
+      int shaindex = json.indexOf("\"sha\": \"");
+      if(shaindex != -1){
+          int start = shaindex+ 7;
+          int end = json.indexOf("\"", start);
+          return json.substring(start,end);
+      }    
+    } catch (Exception e) {
+      Console.log(e);
+    } 
+    return "";
+  }
+}
